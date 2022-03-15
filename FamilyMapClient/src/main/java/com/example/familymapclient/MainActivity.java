7ce51775f5c5e204b2df05916e2be9cbe0f114ed
jharwood <jackson.harwood21@gmail.com>
2022-03-15 11:05:02 -0600

    Login

--- FamilyMapClient/src/main/java/com/example/familymapclient/MainActivity.java
+++ FamilyMapClient/src/main/java/com/example/familymapclient/MainActivity.java
@@ -13,14 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        Button registerButton = findViewById(R.id.registerButton);
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
     }
 
 }

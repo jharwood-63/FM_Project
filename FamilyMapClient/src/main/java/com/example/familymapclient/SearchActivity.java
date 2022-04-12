52bdf3bbe5a3db21268ff436de49d22bf785757c
jharwood-63 <83321059+jharwood-63@users.noreply.github.com>
2022-04-12 11:20:31 -0600

    just working on search algorithm

--- FamilyMapClient/src/main/java/com/example/familymapclient/SearchActivity.java
+++ FamilyMapClient/src/main/java/com/example/familymapclient/SearchActivity.java
@@ -1,12 +1,16 @@
 package com.example.familymapclient;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +19,7 @@ import com.joanzapata.iconify.fonts.FontAwesomeIcons;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -39,21 +44,108 @@ public class SearchActivity extends AppCompatActivity {
         List<Event> filteredEvents = dataCache.getFilteredEvents();
         List<Person> personList = dataCache.getPersonList();
 
+        //List<Event> searchedEvents = searchEvents(filteredEvents);
+        //List<Person> searchedPersons = searchPersons(personList);
+        //EditText searchField = (EditText) findViewById(R.id.searchField);
+        //convert the searchKey to lower case
 
+        /* Search Algorith:
+         * recycler view is empty before a search is executed
+         * people come before events
+         * Make a copy of both lists
+         * make the lists smaller based on what is searched
+         * send those lists to the recycler view
+         */
     }
 
-    //EventPersonAdapter goes here
+    private List<Event> searchEvents(List<Event> events, String searchKey) {
+        List<Event> searchedEvent = new ArrayList<>();
 
-    private class EventPersonViewHandler extends RecyclerView.ViewHolder implements View.OnClickListener {
+        /* What to search for:
+         * Case is ignored
+         * countries, cities, event types, and years
+         */
+
+        for (Event event : events) {
+
+        }
+
+        return null;
+    }
+
+    private boolean checkEvent(Event event, String searchKey) {
+        String country = event.getCountry().toLowerCase();
+        String city = event.getCity().toLowerCase();
+        String eventType = event.getEventType().toLowerCase();
+        String year = String.valueOf(event.getYear()).toLowerCase();
+
+        return country.contains(searchKey) || city.contains(searchKey) || eventType.contains(searchKey) || year.contains(searchKey);
+    }
+
+    private List<Person> searchPersons(List<Person> persons, String searchKey) {
+        /* What to search for:
+         * Case is ignored
+         * first and last names
+         */
+
+        return null;
+    }
+
+    private class EventPersonAdapter extends RecyclerView.Adapter<EventPersonViewHolder> {
+        private final List<Event> searchedEvents;
+        private final List<Person> searchedPersons;
+
+        EventPersonAdapter(List<Event> searchedEvents, List<Person> searchedPersons) {
+            this.searchedEvents = searchedEvents;
+            this.searchedPersons = searchedPersons;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return position < searchedEvents.size() ? EVENT_ITEM_VIEW_TYPE : PERSON_ITEM_VIEW_TYPE;
+        }
+
+        @NonNull
+        @Override
+        public EventPersonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view;
+
+            if (viewType == EVENT_ITEM_VIEW_TYPE) {
+                view = getLayoutInflater().inflate(R.layout.life_event_item, parent, false);
+            }
+            else {
+                view = getLayoutInflater().inflate(R.layout.family_item, parent, false);
+            }
+
+            return new EventPersonViewHolder(view, viewType);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull EventPersonViewHolder holder, int position) {
+            if (position < searchedEvents.size()) {
+                holder.bind(searchedEvents.get(position));
+            }
+            else {
+                holder.bind(searchedPersons.get(position - searchedEvents.size()));
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return searchedEvents.size() + searchedPersons.size();
+        }
+    }
+
+    private class EventPersonViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private final int viewType;
         private Event event;
         private Person person;
 
-        private ImageView marker;
-        private TextView descriptionOne;
-        private TextView descriptionTwo;
+        private final ImageView marker;
+        private final TextView descriptionOne;
+        private final TextView descriptionTwo;
 
-        EventPersonViewHandler(View view, int viewType) {
+        EventPersonViewHolder(View view, int viewType) {
             super(view);
             this.viewType = viewType;
 
@@ -88,12 +180,18 @@ public class SearchActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View view) {
+            Intent intent;
+
             if (viewType == EVENT_ITEM_VIEW_TYPE) {
-                //go to eventActivity
+                intent = new Intent(SearchActivity.this, EventActivity.class);
+                intent.putExtra(getString(R.string.event_id), event.getEventID());
             }
             else {
-                //go to personActivity
+                intent = new Intent(SearchActivity.this, PersonActivity.class);
+                intent.putExtra("personID", person.getPersonID());
             }
+
+            startActivity(intent);
         }
 
         private float decideColor(String eventType) {
